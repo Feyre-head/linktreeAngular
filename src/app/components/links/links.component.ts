6e0637787239f017974f2f7a423f09a8abd2a362
Feyre_head <inaraoenning@gmail.com>
2025-07-08 21:56:01 -0300

    Criando componentes

--- src/app/components/links/links.component.ts
+++ src/app/components/links/links.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-links',
   imports: [],
+  standalone: true,
   templateUrl: './links.component.html',
-  styleUrl: './links.component.scss'
+  styleUrls: ['./links.component.scss'],
 })
-export class LinksComponent {
-
-}
+export class LinksComponent {}
